006956f795d7f156f92c62bc4917ee3bc8ac85c9
“siddiq76” <siddiq.khan0786@gmail.com>
2021-03-08 12:24:50 +0530

    add

---
+++ Census_analyser/src/main/java/com/Census_analyser/Census_analyser/App.java
@@ -0,0 +1,13 @@
+package com.Census_analyser.Census_analyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
